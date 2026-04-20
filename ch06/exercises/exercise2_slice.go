caaa12824e0d5083ea9c56110cbe231305b6b2f7
Furky <ffurkanyilmaz1@gmail.com>
2026-04-20 19:10:27 +0200

    ch06: exercise2 - UpdateSlice and GrowSlice slice behavior

--- ch06/exercises/exercise2_slice.go
+++ ch06/exercises/exercise2_slice.go
@@ -1,13 +1,29 @@
-package exercises
+package main
 
-// Exercise 2:
-// Write a function called fillAndDouble.
-// It should:
-// - take a slice of ints
-// - double every value in the slice
-// - return nothing (modify in place)
-// Think: do you need a pointer to modify a slice's contents?
+import "fmt"
 
-func fillAndDouble(nums []int) {
-	// döngü ile her elemanı 2 ile çarp
+func UpdateSlice(s []string, val string) {
+	// set LAST element to val
+	// then print s inside this function
+	s[len(s)-1] = val
+	fmt.Println(s)
+}
+
+func GrowSlice(s []string, val string) {
+	// append val to s
+	// then print s inside this function
+	s = append(s, val)
+	fmt.Println(s)
+}
+
+func main() {
+	s := []string{"a", "b", "c"}
+
+	fmt.Println("Before UpdateSlice:", s)
+	UpdateSlice(s, "X")
+	fmt.Println("After UpdateSlice:", s)
+
+	fmt.Println("Before GrowSlice:", s)
+	GrowSlice(s, "Y")
+	fmt.Println("After GrowSlice:", s)
 }
