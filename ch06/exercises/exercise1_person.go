b8bd72fed614060bfa78f55d2f77786119191e0c
Furky <ffurkanyilmaz1@gmail.com>
2026-04-20 17:09:19 +0200

    chore(ch06): add exercise skeletons for Person, slice, and GC escape analysis

--- ch06/exercises/exercise1_person.go
+++ ch06/exercises/exercise1_person.go
@@ -1,18 +1,39 @@
-package exercises
+package main
+
+import "fmt"
 
 type Person struct {
 	// Üç alan: FirstName, LastName string | Age int
+	FirstName string
+	LastName  string
+	Age       int
 }
 
 func MakePerson(firstName, lastName string, age int) Person {
 	// Person döndür (pointer değil)
+	return Person{
+		FirstName: firstName,
+		LastName:  lastName,
+		Age:       age,
+	}
 }
 
 func MakePersonPointer(firstName, lastName string, age int) *Person {
-	// *Person döndür
+	// *Person dondurur
+	return &Person{
+		FirstName: firstName,
+		LastName:  lastName,
+		Age:       age,
+	}
 }
 
 func main() {
-	// her iki fonksiyonu çağır
-	// sonuçları bir değişkene ata
+
+	p1 := MakePerson("Ali", "Yılmaz", 25)
+	p2 := MakePersonPointer("Ayşe", "Kaya", 30)
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+	fmt.Printf("%p\n", p2) //Real memory address
+
 }
